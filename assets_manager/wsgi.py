bbb4005020981986a579a07fca290ef0776cf25b
Robin Winslow <robin.winslow@canonical.com>
2014-07-12 00:48:47 +0100

    Add Cling/dj_static support for serving static files in production

--- assets_manager/wsgi.py
+++ assets_manager/wsgi.py
@@ -15,7 +15,7 @@ sys.path.append(BASE_DIR)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "assets_manager.settings")
 
-
-
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+
+application = Cling(get_wsgi_application())
