203b93ad7ce6e67db1f51df7cb7e392dff89496b
Robin Winslow <robin@robinwinslow.co.uk>
2014-09-27 19:40:27 +0100

    Add dev_settings
    - Split out DEBUG django-extensions into dev_settings

--- assets_manager/settings.py
+++ assets_manager/settings.py
@@ -1,86 +1,30 @@
 """
-Django settings for assets_manager project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
+Assets manager settings
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
+# Keep it secret, keep it safe!
+# Although it probably doesn't matter for this app...
 SECRET_KEY = '3z7qsr3n^@dhyb3qh_x_1c#6of_^d=uovy+a7)9sst))ns(697'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    '0.0.0.0', '127.0.0.1', 'localhost',
+    '*.ubuntu.qa', '*.ubuntu.com', 'ubuntu.com'
+]
 
 # Application definition
-
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+INSTALLED_APPS = []
+MIDDLEWARE_CLASSES = []
 
 ROOT_URLCONF = 'assets_manager.urls'
-
 WSGI_APPLICATION = 'assets_manager.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+USE_I18N = False
+USE_L10N = False
+USE_TZ = False
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
-
-TEMPLATE_DIRS = BASE_DIR + '/templates/'
-TEMPLATE_CONTEXT_PROCESSORS = ('django.core.context_processors.request',)
+STATICFILES_DIRS = ["static"]
+TEMPLATE_DIRS = ["templates"]
+TEMPLATE_CONTEXT_PROCESSORS = ['django.core.context_processors.request']
