c630a02dd088673c017f28a9fb533fd7efc505d5
Tristram Oaten <tristram@oaten.name>
2015-07-02 17:14:39 +0100

    Add SSO

--- assets_manager/settings.py
+++ assets_manager/settings.py
@@ -2,6 +2,7 @@
 Assets manager settings
 """
 import os
+import sys
 
 # Keep it secret, keep it safe!
 # Although it probably doesn't matter for this app...
@@ -12,8 +13,21 @@ DEBUG = os.environ.get('WSGI_DEBUG', "").lower() == 'true'
 ALLOWED_HOSTS = ['*']
 
 # Application definition
-INSTALLED_APPS = []
-MIDDLEWARE_CLASSES = []
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django_openid_auth',
+]
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
 
 ROOT_URLCONF = 'assets_manager.urls'
 WSGI_APPLICATION = 'assets_manager.wsgi.application'
@@ -39,6 +53,53 @@ SERVER_URL = os.environ.get('WEBSERVICE_URL', DEFAULT_SERVER_URL)
 AUTH_TOKEN = os.environ.get('AUTH_TOKEN')
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'assets_manager',
+        'USER': 'postgres',
+        'PASSWORD': 'dev',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+# Update database settings from DATABASE_URL environment variable
+import dj_database_url
+DATABASES['default'].update(dj_database_url.config())
+
+if 'test' in sys.argv:
+    DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3'}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-uk'
+
+TIME_ZONE = 'Europe/London'
+
+USE_I18N = False
+
+USE_L10N = False
+
+USE_TZ = True
+
+AUTHENTICATION_BACKENDS = (
+    'django_openid_auth.auth.OpenIDBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+LOGIN_URL = '/openid/login/'
+LOGIN_REDIRECT_URL = '/'
+
+OPENID_CREATE_USERS = True
+OPENID_SSO_SERVER_URL = 'https://login.launchpad.net/'
+OPENID_LAUNCHPAD_TEAMS_REQUIRED = ['canonical-content-people']
+OPENID_USE_AS_ADMIN_LOGIN = True
+OPENID_LAUNCHPAD_TEAMS_MAPPING_AUTO = True
+
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
 
 LOGGING = {
     'version': 1,
--- assets_manager/urls.py
+++ assets_manager/urls.py
@@ -1,5 +1,5 @@
 # Third party imports
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from django.contrib import admin
 
 # Local imports
@@ -12,6 +12,7 @@ urlpatterns = patterns(
     url(r'^$', index, name='index'),
     url(r'^create$', create, name='create'),
     url(r'^update$', update, name='update'),
+    url(r'^openid/', include('django_openid_auth.urls')),
 )
 
 handler404 = error_404
