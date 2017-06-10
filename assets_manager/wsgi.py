e69f174232e9e42e9d09a3559b59bffb59df8363
Robin Winslow <robin@robinwinslow.co.uk>
2017-06-10 11:22:29 +0100

    Fix wsgi server

--- assets_manager/wsgi.py
+++ assets_manager/wsgi.py
@@ -8,7 +8,7 @@ import sys
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(BASE_DIR)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webapp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "assets_manager.settings")
 
 from django.core.wsgi import get_wsgi_application  # noqa: E402
 from whitenoise.django import DjangoWhiteNoise  # noqa: E402
