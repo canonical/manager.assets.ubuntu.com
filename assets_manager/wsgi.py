668e3237c459f0a0d98aef1184685896692b6875
robin@robinwinslow.co.uk <Robin Winslow>
2019-03-26 09:08:17 +0000

    Configure whitenoise v4

--- assets_manager/wsgi.py
+++ assets_manager/wsgi.py
@@ -1,23 +1,12 @@
 """
 WSGI config for webapp project.
 """
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-from future import standard_library
 
-standard_library.install_aliases()  # noqa
-
+# Standard library
 import os
-import sys
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-sys.path.append(BASE_DIR)
+# Packages
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "assets_manager.settings")
-
-from django.core.wsgi import get_wsgi_application  # noqa: E402
-from whitenoise.django import DjangoWhiteNoise  # noqa: E402
-
-application = DjangoWhiteNoise(get_wsgi_application())
+application = get_wsgi_application()
